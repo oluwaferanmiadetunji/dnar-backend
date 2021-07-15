81b41fffa0b9fd23049880e72b204445fd16495d
oluwaferanmiadetunji <oluwaferanmiadetunji@gmail.com>
2021-07-16 00:32:54 +0100

    feat: complete CRUD for employee projects

--- src/components/employeeProjects/controller.js
+++ src/components/employeeProjects/controller.js
@@ -2,7 +2,7 @@ const httpStatus = require('http-status');
 const pick = require('../../utils/pick');
 const ApiError = require('../../utils/ApiError');
 const catchAsync = require('../../utils/catchAsync');
-const { Service } = require('./index');
+const Service = require('./service');
 
 const createEmployeeProject = catchAsync(async (req, res) => {
   const employeeProject = await Service.createEmployeeProject(req.body);
